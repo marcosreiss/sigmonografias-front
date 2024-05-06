918b5dbb2bb4718e871898ca9dec043572c66299
leonardo <leonardogarreto@gmail.com>
2024-05-05 21:48:30 -0300

    feat: chakra

--- src/monografia/Create.jsx
+++ src/monografia/Create.jsx
@@ -1,10 +1,14 @@
+import { Input } from "@chakra-ui/react";
+
 export function Create() {
   return (
-    <div>
-      <input type="text" placeholder="título" />
-      <input type="date" placeholder="data da apresentação" />
-      <input type="text" placeholder="quantidade de páginas" />
-      <input type="file" placeholder="pdf" />
+    <div className="w-full flex justify-center">
+      <div className="flex flex-col justify-center mt-6 gap-4">
+        <Input type="text" placeholder="título" width="400px" />
+        <Input type="date" placeholder="data da apresentação" width="400px" />
+        <Input type="text" placeholder="quantidade de páginas" width="400px" />
+        <Input type="file" placeholder="pdf" width="400px" />
+      </div>
     </div>
   );
 }
